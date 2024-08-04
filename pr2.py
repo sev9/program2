30d6a2342fae57bbe0e8b19954a47e567fa5d752
sev9 <92033595+sev9@users.noreply.github.com>
2024-08-04 12:40:06 +0300

    Add files via upload

---
+++ pr2.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+fig=plt.figure()
+
+x=[i for i in range(1,10)]
+y1=np.random.randint(2.0,6,9)
+y2=np.random.randint(2,6,9)
+plt.ylabel('Средний балл')
+plt.xlabel('Класс')
+
+plt.plot(x,y1)
+plt.plot(x,y2)
+
+plt.legend(['мой средний балл','средний балл класса'])
+
+plt.show()
